c7b77fd53c59ec0c48eb8cec8da7cdafbc0993b0
rwsAdrianDerlatka <adrian.derlatka@rhenus.com>
2024-11-29 11:06:38 +0100

    update api.js

--- js/api.js
+++ js/api.js
@@ -4,7 +4,8 @@ fetch(byLogin, {
     method:"POST",
     mode: "cors",
     headers: {
-        "Content-Type": "application/json"   
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "https://app-dev-350.backend.bywms.rhenus.com:21600/api/platform/login"   
     },
     body: JSON.stringify({
         userId: "adrian.derlatka",
