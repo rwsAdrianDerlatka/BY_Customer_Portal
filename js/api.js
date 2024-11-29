093e36d55c7c2436bce7e7390037479ca4bc87fd
Adrian Derlatka <adrian.derlatka@rhenus.com>
2024-11-29 11:12:11 +0100

    cert added

--- js/api.js
+++ js/api.js
@@ -1,12 +1,11 @@
-const byLogin="https://app-dev-350.backend.bywms.rhenus.com:21600/api/platform/login";
-
 console.log("test")
 
-fetch(byLogin, {
+fetch("https://app-dev-350.backend.bywms.rhenus.com:21600/api/platform/login", {
     method:"POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
+    mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+        },
     body: JSON.stringify({
         userId: "adrian.derlatka",
         password: "N@dk0m1s@rz",
